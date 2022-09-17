12d0f8dbdd8b9d37795a1d905963ce581e1dce07
HOME <jeonghj27@naver.com>
2022-09-17 23:00:08 +0900

    1

---
+++ src/level1/SolutionTest14.java
@@ -0,0 +1,30 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SolutionTest14 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+
+//숫자 내림차순으로 배열하기
+class NumOrder{
+	public long sol(long n) {
+		long answer = 0;
+		
+		String[] s = String.valueOf(n).split("");
+		
+		
+		Arrays.sort(s);
+		
+		
+		
+        return answer;
+	}
+}
