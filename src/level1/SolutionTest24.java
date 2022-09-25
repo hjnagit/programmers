01e973e055ba7af06ca276c0c84003d05e52c1dc
HOME <jeonghj27@naver.com>
2022-09-25 22:12:03 +0900

    기본 문자열 다루기

--- src/level1/SolutionTest24.java
+++ src/level1/SolutionTest24.java
@@ -3,24 +3,51 @@ package level1;
 public class SolutionTest24 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StrBasic sb = new StrBasic();
+		
+		System.out.println(sb.sol("22293"));
+		
+		
 	}
 
 }
 
-
 //문자열 다루기 기본 
-class StrBasic{
+class StrBasic {
 	public boolean sol(String s) {
+
+		String[] str = s.split("");
 		
-		for(int i=0; i<10; i++) {
-			if(!(s.contains(i+""))) {
-				return false;
-			}
+		int num = 0;
+		if(str.length != 4 || str.length != 6) {
+			return false;
 		}
 		
-		
+		for (int j = 0; j < str.length; j++) {
+			for (int i = 0; i < 10; i++) {
+				if (str[j].equals(i + "")) {
+					num = 1;
+				}
+				System.out.println(j + "    " + i);
+			}
+			if(num != 1) {
+				return false;
+			}
+			num = 0;
+		}
+
 		return true;
 	}
+	
+	public boolean solution1(String s) {
+	      if(s.length() == 4 || s.length() == 6){
+	          try{
+	              int x = Integer.parseInt(s);
+	              return true;
+	          } catch(NumberFormatException e){
+	              return false;
+	          }
+	      }
+	      else return false;
+	  }
 }
