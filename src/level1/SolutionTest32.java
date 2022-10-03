89a8f3b79949cd5d5859ff591b082506cf5d9133
HOME <jeonghj27@naver.com>
2022-10-03 21:47:40 +0900

    행렬의 덧셈

--- src/level1/SolutionTest32.java
+++ src/level1/SolutionTest32.java
@@ -17,18 +17,15 @@ public class SolutionTest32 {
 }
 
 
-//Çà·ÄÀÇ µ¡¼À - Çª´Â ÁßÀÔ´Ï´ç - ÀÌ~°Ô~¹¹¾ß~?
+//Çà·ÄÀÇ µ¡¼À
 class Sum32{
 	public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1[0].length][arr1[1].length];
-        System.out.println(arr1[0].length + ", " + arr1[1].length);
-        System.out.println(answer[0].length + ", " + answer[1].length);
-        System.out.println("answer.length : " + arr1.length);
+        int[][] answer = new int[arr1.length][arr1[0].length];
         
         for(int i=0; i<answer.length; i++) {
         	for(int j=0; j<answer[i].length; j++) {
         		answer[i][j] = arr1[i][j] + arr2[i][j];
-        		System.out.println(answer[i][j]);
+        		//System.out.println(answer[i][j]);
         	}
         }
         
