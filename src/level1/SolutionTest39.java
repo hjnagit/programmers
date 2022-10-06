b6ab6f7d8829f0d47e35a27c4af0d790ec31b5b3
HOME <jeonghj27@naver.com>
2022-10-06 23:30:14 +0900

    k번째 수

---
+++ src/level1/SolutionTest39.java
@@ -0,0 +1,37 @@
+package level1;
+
+import java.util.Arrays;
+
+public class SolutionTest39 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static int[] solution(int[] array, int[][] commands) {
+
+		int a, b, c;
+
+		int[] answer = new int[commands.length];
+		// System.out.println(commands.length);
+
+		for (int i = 0; i < commands.length; i++) {
+			a = commands[i][0];
+			b = commands[i][1];
+			c = commands[i][2];
+
+			int[] arr = Arrays.copyOfRange(array, a - 1, b);
+			Arrays.sort(arr);
+
+			// for(int aa : arr) {
+			// System.out.print(aa);
+			// }
+			// System.out.println();
+
+			answer[i] = arr[c - 1];
+		}
+
+		return answer;
+	}
+}
