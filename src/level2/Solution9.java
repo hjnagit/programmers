fbef70dc4ebcf81273f3a52d91f5f9d7ad7fb23f
jhhhdev <jeonghj27@naver.com>
2022-10-29 20:58:14 +0900

    짝지어 제거하기

--- src/level2/Solution9.java
+++ src/level2/Solution9.java
@@ -1,10 +1,24 @@
 package level2;
 
+import java.util.Stack;
+
 public class Solution9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "3r3sdgdy4ytesgseg";
+		solution(str);
 	}
+	
+	//짝지어 제어하기
+	static int solution(String s)
+    {
+		Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()) 
+          if(!stack.isEmpty() && stack.peek() == c) stack.pop();
+          else stack.push(c);
+         
+        return stack.isEmpty() ? 1 : 0;
+    }
 
 }
